201c87602223b7c2d0d7df8fcdb7434ea58fc0b8
SunLawnY <sunnylaw3571@gmail.com>
2024-06-10 10:55:13 +0100

    test hide the username password

--- src/main/resources/SQLinsertExample.sql
+++ src/main/resources/SQLinsertExample.sql
@@ -7,13 +7,27 @@ CREATE TABLE IF NOT EXISTS Album (
     stock INT
 );
 
-INSERT INTO Album (id, artist, release_year, genre, album_name, stock) VALUES (1, 'The Beatles', 1969, 'ROCK', 'Abbey Road', 10);
-INSERT INTO Album (id, artist, release_year, genre, album_name, stock) VALUES (2, 'Michael Jackson', 1982, 'POP', 'Thriller', 15);
-INSERT INTO Album (id, artist, release_year, genre, album_name, stock) VALUES (3, 'Pink Floyd', 1973, 'ROCK', 'The Dark Side of the Moon', 20);
+INSERT INTO Album (id, artist, release_year, genre, album_name, stock)
+VALUES (1, 'The Beatles', 1969, 'ROCK', 'Abbey Road', 10);
+
+INSERT INTO Album (id, artist, release_year, genre, album_name, stock)
+VALUES (2, 'Michael Jackson', 1982, 'POP', 'Thriller', 15);
+
+INSERT INTO Album (id, artist, release_year, genre, album_name, stock)
+VALUES (3, 'Pink Floyd', 1973, 'ROCK', 'The Dark Side of the Moon', 20);
+
 INSERT INTO Album (id, artist, release_year, genre, album_name, stock) VALUES (4, 'Led Zeppelin', 1971, 'ROCK', 'Led Zeppelin IV', 12);
+
 INSERT INTO Album (id, artist, release_year, genre, album_name, stock) VALUES (5, 'Nirvana', 1991, 'ROCK', 'Nevermind', 18);
+
 INSERT INTO Album (id, artist, release_year, genre, album_name, stock) VALUES (6, 'Madonna', 1984, 'POP', 'Like a Virgin', 25);
+
 INSERT INTO Album (id, artist, release_year, genre, album_name, stock) VALUES (7, 'Prince', 1984, 'POP', 'Purple Rain', 22);
+
 INSERT INTO Album (id, artist, release_year, genre, album_name, stock) VALUES (8, 'Fleetwood Mac', 1977, 'ROCK', 'Rumours', 30);
+
 INSERT INTO Album (id, artist, release_year, genre, album_name, stock) VALUES (9, 'Eagles', 1976, 'ROCK', 'Hotel California', 14);
+
 INSERT INTO Album (id, artist, release_year, genre, album_name, stock) VALUES (10, 'AC/DC', 1980, 'ROCK', 'Back in Black', 17);
+
+-- \i /home/sunny-law/Desktop/NorthCoders/20240607_MiniProject_RecordShopAPI/RecordShopAPI/src/main/resources/SQLinsertExample.sql
